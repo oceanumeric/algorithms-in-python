d2232dbe6a63701c82c530aeb0d84c5f5516ab7a
oceanumeric <numerical.ocean@gmail.com>
2021-11-22 13:09:31 +0100

    [add] c5 e1 py

---
+++ gtg/c5_e1.py
@@ -0,0 +1,66 @@
+"""Dynamic arrays and Amortization"""
+import sys
+import ctypes
+
+
+class DynamicArray:
+    """A dynamic array class akin to a simplified python list."""
+    
+    def __init__(self):
+        """Create an empty array"""
+        self._n = 0
+        self._capacity = 1
+        self._A = self._make_array(self._capacity)
+        
+    def __len__(self):
+        return self._n
+    
+    def __getitem__(self, k):
+        if not 0 <= k < self._n:
+            raise IndexError("Invalid Index") 
+        return self._A[k]
+    
+    def append(self, obj):
+        """Add object to end of the array"""
+        if self._n == self._capacity:
+            self._resize(2 * self._capacity)
+        self._A[self._n] = obj
+        self._n += 1 
+        
+    def _resize(self, c):
+        """Resize internal array to capacity c"""
+        B = self._make_array(c)
+        for k in range(self._n):
+            B[k] = self._A[k]
+        self._A = B 
+        self._capacity = c 
+        
+    def _make_array(self, c):
+        """Return new array with capacity c"""
+        return (c * ctypes.py_object)()
+    
+    def __str__(self):
+        return '[' + "".join(str(self._A[i]) for i in range(self._n)) + ']'
+            
+
+
+if __name__ == "__main__":
+    data = []
+    for k in range(10):
+        a = len(data)
+        b = sys.getsizeof(data)
+        # the number of bytes jump 32 dynamically 
+        print(f"Length: {a:2d}; Size in bytes: {b:4d}")
+        data.append(None)
+    print("------------------------------------")
+    da = DynamicArray()
+    for k in range(10):
+        a = len(da)
+        b = sys.getsizeof(da)
+        # the number of bytes jump 32 dynamically 
+        print(f"Length: {a:2d}; Size in bytes: {b:4d}")
+        da.append(2)
+        print(da._capacity, da._n)
+    print(da)
+    print(da[3])
+    
